9c2a1af3f16920f8e18a1c469d5fee614f335733
Emanuel Vidal <emanuelvidal@DTECM0965048173.acad.unifor.br>
2023-10-11 14:10:48 -0300

    :lipstick: and :sparkles: fixed styling and new property component

---
+++ src/PropertyCard.tsx
@@ -0,0 +1,26 @@
+interface PropertyCardProps {
+  key: number;
+  city: string;
+  state: string;
+  street: string;
+  neighborhood: string;
+  zipcode: number;
+}
+
+export default function PropertyCard({
+  city,
+  state,
+  street,
+  neighborhood,
+  zipcode,
+}: PropertyCardProps) {
+  return (
+    <div className='border border-slate-200 rounded-xl w-[80%] h-40'>
+      <h1>{neighborhood}</h1>
+      <h1>{street}</h1>
+      <h1>{city}</h1>
+      <h1>{state}</h1>
+      <h1>{zipcode}</h1>
+    </div>
+  );
+}
